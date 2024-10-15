cc1a8de75faa2943f94d1e68169ed73864d7aba0
Elman Islam <eislam@uoguelph.ca>
2024-10-15 15:40:52 -0400

    Add q1 directory

---
+++ src/q1/PrimeNumbersTest.java
@@ -0,0 +1,60 @@
+/*
+ * 
+ * Author: Elman I.
+ * Email: elmanislam123@gmail.com
+ * 
+ * Creation Date: 2024-10-14 21:13:25
+ Last Modification Date: 2024-10-15 15:21:22
+ * 
+ */
+
+import org.junit.Test;
+import java.util.Vector;
+
+import static org.junit.Assert.*;
+
+public class PrimeNumbersTest {
+
+   /* (a) A test that does not reach the fault */
+   @Test
+   public void testWithZeroInput() {
+      PrimeNumbers primeNumbers = new PrimeNumbers();
+      primeNumbers.computePrimes(0);
+
+      assertEquals("[]", primeNumbers.toString());
+      // it never enters the while loop
+   }
+
+   /* (b) A test that reaches the fault, but does not infect */
+   @Test
+   public void testWithoutInfecting() {
+      PrimeNumbers primeNumbers = new PrimeNumbers();
+      primeNumbers.computePrimes(7);
+
+      assertEquals("[2, 3, 5, 7, 11, 13, 17]", primeNumbers.toString());
+      // The code where the fault exists is reached. However, it is never 'infected'
+      // As the condition to infect does not activate, due to the first prime number
+      // ending with a 9 not being reached (i.e. '19').
+   }
+
+   /* (c) A test that infects the state, but does not propagate */
+   @Test
+   public void testInfectionWithoutPropagation() {
+      PrimeNumbers primeNumbers = new PrimeNumbers();
+      primeNumbers.computePrimes(8);
+
+      assertEquals("[2, 3, 5, 7, 11, 13, 17, 19]",
+            primeNumbers.toString());
+   }
+
+   /* (e) A test that reveals the fault */
+   @Test
+   public void revealFault() {
+      PrimeNumbers primeNumbers = new PrimeNumbers();
+      primeNumbers.computePrimes(8);
+
+      assertEquals("[2, 3, 5, 7, 11, 13, 17, 19]", primeNumbers.toString());
+      // The code where the fault exists is reached. A fault is revealed as 19 should
+      // be present in the output, but is not received.
+   }
+}
